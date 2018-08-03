dcbfb49ebd71cb6f19defcc3474fb2ef6af91b50
YONATAN GOSSAYE <yonimethodic@gmail.com>
2018-08-03 11:29:41 +0300

    HRM Core Domain Folder with Employee and Emergency Contact

---
+++ Chai.WorkflowManagment.CoreDomain/HRM/EmergencyContact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chai.WorkflowManagment.CoreDomain.Setting;
+using Chai.WorkflowManagment.CoreDomain.Users;
+
+namespace Chai.WorkflowManagment.CoreDomain.Requests
+{
+    public partial class EmergencyContact : IEntity
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string SubCity { get; set; }
+
+        public string Woreda { get; set; }
+        public string HouseNo { get; set; }
+        public string TelephoneHome { get; set; }
+        public string TelephoneOffice { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual AppUser AppUser { get; set; }
+        public virtual VehicleRequest VehicleRequest { get; set; }
+    }
+}
---
+++ Chai.WorkflowManagment.CoreDomain/HRM/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chai.WorkflowManagment.CoreDomain.Setting;
+using Chai.WorkflowManagment.CoreDomain.Users;
+
+namespace Chai.WorkflowManagment.CoreDomain.HRM
+{
+    public class Employee :IEntity
+    {
+        public int Id { get; set; }
+    }
+}
