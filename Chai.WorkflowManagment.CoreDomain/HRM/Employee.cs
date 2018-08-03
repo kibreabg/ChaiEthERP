c71ef553a0415566bbda9f7dc03532f7abde872c
Dereje Haddis <dereje.haddis@gmail.com>
2018-08-03 12:30:58 +0300

    Employee

--- Chai.WorkflowManagment.CoreDomain/HRM/Employee.cs
+++ Chai.WorkflowManagment.CoreDomain/HRM/Employee.cs
@@ -10,5 +10,24 @@ namespace Chai.WorkflowManagment.CoreDomain.HRM
     public class Employee :IEntity
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get;  set; }
+        public DateTime DateOfBirth { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Nationality { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string  Country { get; set; }
+        public string Phone { get; set; }
+        public string CellPhone { get; set; }
+        public string PersonalEmail { get; set; }
+        public string ChaiEMail { get; set; }
+        public string Photo { get; set; }
+        public Boolean Status { get; set; }
+
+
+
+
     }
 }
---
+++ Chai.WorkflowManagment.CoreDomain/HRM/EmployeeDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chai.WorkflowManagment.CoreDomain.Setting;
+using Chai.WorkflowManagment.CoreDomain.Users;
+
+namespace Chai.WorkflowManagment.CoreDomain.Requests
+{
+    public partial class EmployeeDetail : IEntity
+    {
+        public int Id { get; set; }
+        public virtual Setting.JobTitle JobTitle { get; set; }
+       
+        public virtual Setting.Position Position { get; set; }
+
+        public virtual Setting.Program Program { get; set; }
+
+        public int  DutyStation { get; set; }
+
+        public decimal Salary { get;  set;}
+
+        public int EmploymentStatus { get; set; }
+        public int Class { get; set; }
+        public string HoursPerWeek { get; set; }
+        public string BaseCountry { get; set; }
+        public string BaseCity { get; set; }
+        public string BaseState { get; set; }
+        public string CountryTeam { get; set; }
+        public string DescriptiveJobTitle { get; set; }
+        public int Supervisor { get; set; }
+        public int ReportsTo { get; set; }
+        
+            
+
+
+
+        public virtual HRM.Employee Employee { get; set; }
+    
+    }
+}
