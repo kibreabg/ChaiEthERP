7ebeb8ae1f17e0672b67282fe7f0d26b9b9a36b9
gbirega <gbirega@ET-PC072Y3B>
2018-08-09 16:03:43 +0300

    Termination,WorkExperience and Termination reason objects

---
+++ Chai.WorkflowManagment.CoreDomain/HRM/Termination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chai.WorkflowManagment.CoreDomain.HRM
+{
+    public  class Termination : IEntity
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime TerminationDate { get; set; }
+        public DateTime LastDateOfEmployee { get; set; }
+        public string RecommendationforRehire { get; set; }
+        public string TerminationReasonId { get; set; }
+       
+    }
+}
