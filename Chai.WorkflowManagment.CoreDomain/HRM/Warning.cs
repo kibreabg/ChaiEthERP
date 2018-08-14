374da0df62db40f383055913448130858457376c
Dereje Haddis <dereje.haddis@gmail.com>
2018-08-14 11:26:16 +0300

    Warning Object

---
+++ Chai.WorkflowManagment.CoreDomain/HRM/Warning.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chai.WorkflowManagment.CoreDomain.Setting;
+using Chai.WorkflowManagment.CoreDomain.Users;
+
+namespace Chai.WorkflowManagment.CoreDomain.HRM
+{
+    public partial class Warning : IEntity
+    {
+        public int Id { get; set; }
+
+        public string WarningDescription { get; set; }
+        public DateTime WarningDate { get; set; }
+          
+        public virtual HRM.Employee Employee { get; set; }
+
+    }
+}
