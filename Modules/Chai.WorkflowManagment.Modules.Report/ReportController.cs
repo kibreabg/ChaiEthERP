07362e5217acae16620cce92d67c61b7e9e95722
YONATAN GOSSAYE <yonimethodic@gmail.com>
2019-12-27 11:58:36 +0300

    Employee List update

--- Modules/Chai.WorkflowManagment.Modules.Report/ReportController.cs
+++ Modules/Chai.WorkflowManagment.Modules.Report/ReportController.cs
@@ -225,7 +225,7 @@ namespace Chai.WorkflowManagment.Modules.Report
         {
             string filterExpression = "";
 
-            filterExpression = "SELECT * FROM Employees left Join Contracts on Contracts.Employee_Id=Employees.Id Inner Join AppUsers on Appusers.Id = Employees.Id left Join EmployeeDetails on EmployeeDetails.Contract_Id = Contracts.Id and EmployeeDetails.Id = (SELECT MAX(Id) FROM EmployeeDetails)  Where 1 = Case when '" + FullName + "' = '' Then 1 When (Employees.FirstName + ' ' + Employees.lastName) like '%" + FullName + "%' Then 1 END ";
+            filterExpression = "SELECT * FROM Employees left Join Contracts on Contracts.Employee_Id=Employees.Id and Contracts.Status='Active' Inner Join AppUsers on Appusers.Id = Employees.Id left Join EmployeeDetails on EmployeeDetails.Contract_Id = Contracts.Id and EmployeeDetails.Id = (SELECT MAX(Id) FROM EmployeeDetails)  Where 1 = Case when '" + FullName + "' = '' Then 1 When (Employees.FirstName + ' ' + Employees.lastName) like '%" + FullName + "%' Then 1 END order by Employees.FirstName";
             // return WorkspaceFactory.CreateReadOnly().Queryable<CashPaymentRequest>(filterExpression).ToList();
             return _workspace.SqlQuery<Employee>(filterExpression).ToList();
         }
