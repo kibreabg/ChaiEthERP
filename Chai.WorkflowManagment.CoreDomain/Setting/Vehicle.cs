bd169a7bc182d72a6163ec8c57e800e51bef0a72
Dereje Haddis <dereje.haddis@gmail.com>
2020-05-11 13:59:56 +0300

    Vehicle Coredomain

--- Chai.WorkflowManagment.CoreDomain/Setting/Vehicle.cs
+++ Chai.WorkflowManagment.CoreDomain/Setting/Vehicle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Chai.WorkflowManagment.CoreDomain.Request;
+using Chai.WorkflowManagment.CoreDomain.Users;
 
 namespace Chai.WorkflowManagment.CoreDomain.Setting
 {
@@ -10,7 +11,19 @@ namespace Chai.WorkflowManagment.CoreDomain.Setting
     {
         public int Id { get; set; }
         public string PlateNo { get; set; }
+        
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int MakeYear { get; set; }
+        public string FrameNumber { get; set; }
+        public string EngineType { get; set; }
+        public string Transmission { get; set; }
+        public string BodyType { get; set; }
+        public string EngineCapacity { get; set; }
+        public int PurchaseYear { get; set; }
+        public int LastKmReading { get; set; }
+        public virtual AppUser AppUser { get; set; }
         public string Status { get; set; }
-       
+
     }
 }
