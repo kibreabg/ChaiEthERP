cc69c1f79121cfe40d58fef7b23dda5ac2719744
gbirega <gbirega@ET-PC072Y3B>
2020-05-21 12:15:14 +0300

    Excluded DB context and webconfig changes

--- Chai.WorkflowManagment.CoreDomain/DataAccess/WorkflowManagmentDbContext.cs
+++ Chai.WorkflowManagment.CoreDomain/DataAccess/WorkflowManagmentDbContext.cs
@@ -19,7 +19,7 @@ namespace Chai.WorkflowManagment.CoreDomain.DataAccess
     public class WorkflowManagmentDbContext : BaseDbContext
     {
         public WorkflowManagmentDbContext(bool disableProxy)
-            : base("CHAIWorkflow")
+            : base("WorkflowTestDB")
         {
             if (disableProxy)
                 ObjContext().ContextOptions.ProxyCreationEnabled = false;
