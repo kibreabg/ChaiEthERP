0abe5296b2b3a7916e8898d6c80c11587c43f2ea
kibreabg <kibreabg@gmail.com>
2020-09-22 12:03:21 +0300

    Bank Payment & CashPayment Bug fixes

--- Chai.WorkflowManagment.CoreDomain/Requests/OperationalControlRequest.cs
+++ Chai.WorkflowManagment.CoreDomain/Requests/OperationalControlRequest.cs
@@ -24,6 +24,7 @@ namespace Chai.WorkflowManagment.CoreDomain.Requests
         public string PaymentType { get; set; }
         public string BankName { get; set; }
         public int CurrentApprover { get; set; }
+        public int CurrentApproverPosition { get; set; }
         public int CurrentLevel { get; set; }
         public string CurrentStatus { get; set; }
         public decimal TotalAmount { get; set; }
